21d27608095c95047429e55315aae3dddbd49449
Kristian <dalmo@surface>
2016-05-26 13:19:21 +0200

    copied changes to renamed_vars from reporttoolDT.

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,10 @@
+# http://stackoverflow.com/questions/31522745/capture-a-data-frame-or-non-atomic-object-with-lazyeval
+# f <- function(...) lazyeval::lazy_( substitute(...), parent.frame() )
+# f(iris)
+# f(1:10)
+# a = 1:5
+# f(a)
+
 # Capture dots. Primarily used to pass calls to R6 methods ---------------------
 capture_dots <- function(...) {
   eval(substitute(alist(...)))
@@ -5,15 +12,20 @@ capture_dots <- function(...) {
 
 # Get renamed columns from lazy_dots in dplyr::select and dplyr::rename.
 # Return is equivalent to: setNames(old_name, new_name) ------------------------
-renamed_vars <- function(dots) {
-  expr <- dots[!is.na(names(dots)) & names(dots) != ""]
-  if (length(expr)) {
-    nms <- lapply(names(expr), function(nm) { x <- expr[[nm]]$expr; if (x != nm) x })
-    nms <- setNames(as.character(unlist(nms)), names(expr))
-  } else {
-    nms <- NULL
+renamed_vars <- function(vars, dots) {
+  renamed <- dplyr::select_vars_(vars, dots)
+  renamed <- renamed[names(renamed) != renamed]
+
+  # Return NULL early if nothing has been renamed.
+  if (!length(renamed)) return()
+
+  # If renamed. Return a renamed vars vector.
+  # (has to be done in a loop to support referencing newly created vars.)
+  for (i in seq_along(renamed)) {
+    vars[vars == renamed[i]] <- names(renamed)[i]
   }
-  nms
+  vars
+
 }
 
 # Check whether data is a tbl --------------------------------------------------
